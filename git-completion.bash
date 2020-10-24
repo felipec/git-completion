282bb9bc13b926f5e6fbbc0966570220a63f37ad
Felipe Contreras <felipe.contreras@gmail.com>
2020-10-24 04:52:28 -0500

    Sync fixes
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- git-completion.bash
+++ git-completion.bash
@@ -3637,6 +3637,6 @@ __git_complete gitk __gitk_main
 # when the user has tab-completed the executable name and consequently
 # included the '.exe' suffix.
 #
-if [ "$OSTYPE" = "Cygwin" ]; then
+if [ "$OSTYPE" = cygwin ]; then
 	__git_complete git.exe __git_main
 fi
