40925c073226ab1feaf847416526e46bb626337a
Felipe Contreras <felipe.contreras@gmail.com>
2020-10-24 20:51:15 -0500

    completion: bash: get rid of any non-append code
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- git-completion.bash
+++ git-completion.bash
@@ -300,7 +300,7 @@ __gitcomp_direct ()
 	COMPREPLY+=($1)
 }
 
-__gitcompappend ()
+__gitcompadd ()
 {
 	local x i=${#COMPREPLY[@]}
 	for x in $1; do
@@ -308,12 +308,6 @@ __gitcompappend ()
 			COMPREPLY[i++]="$2$x$4"
 		fi
 	done
-}
-
-__gitcompadd ()
-{
-	COMPREPLY=()
-	__gitcompappend "$@"
 }
 
 # Generates completion reply, appending a space to possible completion words,
@@ -429,7 +423,7 @@ __gitcomp_builtin ()
 __gitcomp_nl ()
 {
 	local IFS=$'\n'
-	__gitcompappend "$1" "${2-}" "${3-$cur}" "${4- }"
+	__gitcompadd "$1" "${2-}" "${3-$cur}" "${4- }"
 }
 
 # Fills the COMPREPLY array with prefiltered paths without any additional
@@ -442,7 +436,7 @@ __gitcomp_file_direct ()
 {
 	local IFS=$'\n'
 
-	COMPREPLY=($1)
+	COMPREPLY+=($1)
 
 	# use a hack to enable file mode in bash < 4
 	compopt -o filenames +o nospace 2>/dev/null ||
