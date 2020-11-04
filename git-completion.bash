754796737af0f5b10ad446824bd50cd11d227c64
Felipe Contreras <felipe.contreras@gmail.com>
2020-11-04 00:24:10 -0600

    completion: bash: simplify _get_comp_words_by_ref()
    We don't need the whole functionality of _get_comp_words_by_ref(), we
    know exactly what we need from that function, so only do that.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- git-completion.bash
+++ git-completion.bash
@@ -408,30 +408,12 @@ __git_reassemble_comp_words_by_ref()
 if ! type _get_comp_words_by_ref >/dev/null 2>&1; then
 _get_comp_words_by_ref ()
 {
-	local exclude cur_ words_ cword_
-	if [ "$1" = "-n" ]; then
-		exclude=$2
-		shift 2
-	fi
-	__git_reassemble_comp_words_by_ref "$exclude"
-	cur_=${words_[cword_]}
-	while [ $# -gt 0 ]; do
-		case "$1" in
-		cur)
-			cur=$cur_
-			;;
-		prev)
-			prev=${words_[$cword_-1]}
-			;;
-		words)
-			words=("${words_[@]}")
-			;;
-		cword)
-			cword=$cword_
-			;;
-		esac
-		shift
-	done
+	local words_ cword_
+	__git_reassemble_comp_words_by_ref "=:"
+	cword=$cword_
+	cur=${words_[cword]}
+	prev=${words_[cword-1]}
+	words=("${words_[@]}")
 }
 fi
 
