5bd3b59566f331814c2900066b1965cd4033b687
Felipe Contreras <felipe.contreras@gmail.com>
2020-11-11 08:39:31 -0600

    completion: zsh: add correct removable suffix
    Zsh has a nice feature that allows a suffix to be automatically removed
    if followed by a space.
    
    For example:
    
      git log --prety=
    
    If a space is typed, the suffix '=' is removed.
    
    But we have to set the correct prefix first.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- git-completion.zsh
+++ git-completion.zsh
@@ -51,7 +51,7 @@ functions[complete]="$old_complete"
 
 __gitcompadd ()
 {
-	compadd -p "${2-}" -S "${3- }" -- ${=1} && _ret=0
+	compadd -p "${2-}" -S "${3- }" -q -- ${=1} && _ret=0
 }
 
 __gitcomp ()
@@ -79,7 +79,8 @@ __gitcomp_opts ()
 
 		if [[ -z "${4+set}" ]]; then
 			case $c in
-			*=|*.) sfx="" ;;
+			*=) c="${c%=}"; sfx="=" ;;
+			*.) sfx="" ;;
 			*) sfx=" " ;;
 			esac
 		else
