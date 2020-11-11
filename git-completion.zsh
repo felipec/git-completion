549f88dfa21be2017660a4d5ff96ef43a9883e09
Felipe Contreras <felipe.contreras@gmail.com>
2020-11-10 20:57:13 -0600

    completion: zsh: add support for general -c opts
    So we can specify configurations.
    
    Signed-off-by: Felipe Contreras <felipe.contreras@gmail.com>

--- git-completion.zsh
+++ git-completion.zsh
@@ -242,6 +242,7 @@ __git_zsh_main ()
 		'--no-replace-objects[do not use replacement refs to replace git objects]' \
 		'(- :)--help[prints the synopsis and a list of the most commonly used commands]: :->arg' \
 		'*-C[run as if git was started in the given path]: :_directories' \
+		'*-c[pass a configuration parameter to the command]: :->config' \
 		'(-): :->command' \
 		'(-)*:: :->arg' && return
 
@@ -254,6 +255,10 @@ __git_zsh_main ()
 			_requested all-commands && __git_zsh_cmd_all
 			let _ret || break
 		done
+		;;
+	(config)
+		compset -P '*[=:]'
+		emulate ksh -c __git_complete_config_variable_name_and_value
 		;;
 	(arg)
 		local command="${words[1]}" __git_dir
